ca714b704ecb70854e827c20258c28bdf741b2e3
Tibaldi3 <francesco.sbk3@gmail.com>
2024-03-25 12:14:57 +0100

    pagina prodotti proto

--- assets/app.js
+++ assets/app.js
@@ -11,8 +11,6 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 
-document.styleSheets[0].addRule('i.badge:after','content: "'+10+'";');
-
 const mesi = (mese) => {
     switch(mese) {
         case 1: return 'Gennaio';
@@ -30,25 +28,40 @@ const mesi = (mese) => {
         default: return 'Mese non valido';
     }
 }
+const currentLocation = window.location.pathname.split("/")[window.location.pathname.split().findLastIndex((e) => e == "/")]
+if (currentLocation == "") {
+    var totalDays = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate();
+    document.getElementById('mese').innerHTML = `Questi sono i 3 prodotti piu venduti a <strong>${mesi(new Date().getMonth()+1)}</strong>`
+
+    var data = [];
+    for (var i = 1; i <= totalDays; i++) {
+        data.push({ day: i.toString(), bicchieri: Math.floor(Math.random() * 50), astucci: Math.floor(Math.random() * 50), penne: Math.floor(Math.random() * 50)}); 
+    }
 
 
-var totalDays = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate();
-document.getElementById('mese').innerHTML = `Questi sono i 3 prodotti piu venduti a <strong>${mesi(new Date().getMonth()+1)}</strong>`
-
-var data = [];
-for (var i = 1; i <= totalDays; i++) {
-    data.push({ day: i.toString(), bicchieri: Math.floor(Math.random() * 50), astucci: Math.floor(Math.random() * 50), penne: Math.floor(Math.random() * 50)}); 
+    new Morris.Area({
+        element: 'chart',
+        data: data,
+        xkey: 'day',
+        ykeys: ['bicchieri', 'astucci', 'penne'],
+        labels: ['Bicchieri', 'Astucci', 'Penne'],
+        parseTime: false,
+        lineColors: ['#198754', '#2A9FD6', '#C19A6B'],
+    });
 }
+//window.addEventListener("DOMContentLoaded", () => {
+//    let imgs = ["{{asset('/imgs/astuccio.jpeg')}}", "{{asset('imgs/astuccio1.jpeg')}}", "{{asset('imgs/bicchiere.jpeg')}}", "{{asset('imgs/bicchiere1.jpeg')}}", "{{asset('imgs/martello.jpeg')}}", "{{asset('imgs/apenna.jpeg')}}", "{{asset('imgs/penna1.jpeg')}}"]
+//    const products = document.querySelector(".products")
 
-
-new Morris.Area({
-    element: 'chart',
-    data: data,
-    xkey: 'day',
-    ykeys: ['bicchieri', 'astucci', 'penne'],
-    labels: ['Bicchieri', 'Astucci', 'Penne'],
-    parseTime: false,
-    lineColors: ['#198754', '#2A9FD6', '#C19A6B'],
-});
+//    for (i=0; i<imgs.length; i++)
+//    {
+//        const div = document.createElement("div")
+//        const img = document.createElement("img")
+//        img.src = imgs[i]
+//        div.appendChild(img)
+//        products.appendChild(div)
+//    }
+  
+//})
 
 
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,14 +12,17 @@ class MainController extends AbstractController
      * @Route("/", name="vendite")
      */
     public function vendite() {
-
         return $this->render("vendite/vendite.html.twig");
     }
     /**
      * @Route("/prodotti", name="prodotti")
      */
     public function index() {
-       return $this->render("products/products.html.twig");
+       return $this->render("products/products.html.twig", [
+        'imgs' => ['imgs/astuccio.jpeg', 'imgs/astuccio1.jpeg', 'imgs/bicchiere.jpeg', 'imgs/bicchiere1.jpeg', 'imgs/penna.jpeg', 'imgs/penna1.jpeg'],
+        'nome' =>['astuccio rosa', 'astuccio blu', 'bicchiere acqua', 'calice', 'penna sfera', 'penna bic'],
+        'prezzo' => ['15,99€', '23,90€', '9,99€', '18,99€', '29,99€', '16,99€'],
+       ]);
     }
     /**
      * @Route("/carrello", name="carrello")
@@ -27,6 +30,7 @@ class MainController extends AbstractController
     public function cart() {
         return $this->render("cart/cart.html.twig");
      }
+
 }
 
 
